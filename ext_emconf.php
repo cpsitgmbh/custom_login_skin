36fa07d7e5bfa408768ec0aea6fdc31c85adc4c3
Nicole Cordes <cordes@cps-it.de>
2014-08-25 22:43:09 +0200

    [RELEASE] Release of custom_login_skin 0.1.0

--- ext_emconf.php
+++ ext_emconf.php
@@ -3,7 +3,7 @@
 /***************************************************************
  * Extension Manager/Repository config file for ext "custom_login_skin".
  *
- * Auto generated 31-05-2013 09:05
+ * Auto generated 25-08-2014 22:42
  *
  * Manual updates:
  * Only the data in the array - everything else is removed by next
@@ -16,36 +16,25 @@ $EM_CONF[$_EXTKEY] = array (
 	'category' => 'be',
 	'author' => 'Nicole Cordes',
 	'author_email' => 'cordes@cps-it.de',
-	'shy' => '',
-	'dependencies' => '',
-	'conflicts' => '',
-	'priority' => '',
-	'module' => '',
 	'state' => 'stable',
-	'internal' => '',
 	'uploadfolder' => 0,
 	'createDirs' => '',
-	'modify_tables' => '',
 	'clearCacheOnLoad' => 0,
-	'lockType' => '',
 	'author_company' => 'CPS-IT GmbH',
 	'version' => '0.1.0',
-	'constraints' =>
+	'constraints' => 
 	array (
-		'depends' =>
+		'depends' => 
+		array (
+			'typo3' => '6.2.0-6.2.99',
+		),
+		'conflicts' => 
 		array (
 		),
-		'conflicts' =>
-		array (
-		),
-		'suggests' =>
+		'suggests' => 
 		array (
 		),
 	),
-	'_md5_values_when_last_written' => '',
-	'suggests' =>
-	array (
-	),
+	'_md5_values_when_last_written' => 'a:16:{s:9:"ChangeLog";s:4:"b23c";s:27:"custom_login_skin_0.1.0.zip";s:4:"e06a";s:21:"ext_conf_template.txt";s:4:"786f";s:12:"ext_icon.gif";s:4:"ab1b";s:17:"ext_localconf.php";s:4:"6ecb";s:51:"Classes/Extensionmanager/Configuration/Textarea.php";s:4:"1043";s:30:"Classes/Hook/LoginFormHook.php";s:4:"c8b8";s:26:"Documentation/Includes.txt";s:4:"6d5f";s:23:"Documentation/Index.rst";s:4:"0ed1";s:26:"Documentation/Settings.yml";s:4:"71cd";s:43:"Documentation/AdministratorManual/Index.rst";s:4:"461d";s:37:"Documentation/Images/BackendLogin.jpg";s:4:"b47a";s:44:"Documentation/Images/IntroductionPackage.png";s:4:"bd5d";s:36:"Documentation/Introduction/Index.rst";s:4:"0158";s:38:"Resources/Private/Templates/Login.html";s:4:"313e";s:30:"Resources/Public/Css/Login.css";s:4:"f5da";}',
 );
 
-?>
