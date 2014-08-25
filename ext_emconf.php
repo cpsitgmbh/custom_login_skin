6221b34fe2751a82587cc4762661b6592821b0f9
Nicole Cordes <cordes@cps-it.de>
2014-08-25 22:17:28 +0200

    [TASK] Add initial extension files

---
+++ ext_emconf.php
@@ -0,0 +1,51 @@
+<?php
+
+/***************************************************************
+ * Extension Manager/Repository config file for ext "custom_login_skin".
+ *
+ * Auto generated 31-05-2013 09:05
+ *
+ * Manual updates:
+ * Only the data in the array - everything else is removed by next
+ * writing. "version" and "dependencies" must not be touched!
+ ***************************************************************/
+
+$EM_CONF[$_EXTKEY] = array (
+	'title' => 'Custom login skin',
+	'description' => 'Change the backend login screen with an own style.',
+	'category' => 'be',
+	'author' => 'Nicole Cordes',
+	'author_email' => 'cordes@cps-it.de',
+	'shy' => '',
+	'dependencies' => '',
+	'conflicts' => '',
+	'priority' => '',
+	'module' => '',
+	'state' => 'stable',
+	'internal' => '',
+	'uploadfolder' => 0,
+	'createDirs' => '',
+	'modify_tables' => '',
+	'clearCacheOnLoad' => 0,
+	'lockType' => '',
+	'author_company' => 'CPS-IT GmbH',
+	'version' => '0.1.0',
+	'constraints' =>
+	array (
+		'depends' =>
+		array (
+		),
+		'conflicts' =>
+		array (
+		),
+		'suggests' =>
+		array (
+		),
+	),
+	'_md5_values_when_last_written' => '',
+	'suggests' =>
+	array (
+	),
+);
+
+?>
